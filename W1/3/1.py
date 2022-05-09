3a327c4558c6c5d129fe929d386e23476643af6d
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-09 08:17:26 +0530

    Number of Enclaves

--- W1/3/1.py
+++ W1/3/1.py
@@ -2,14 +2,15 @@
 # https://leetcode.com/problems/number-of-enclaves/
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        k, q = set(), [(sr, sc)]
-        o = image[sr][sc]
-        while(q):
-            z, y = q.pop(0)
-            k.add((z,y))
-            image[z][y] = newColor
-            for x, w in [(1,0), (0, 1), (-1, 0), (0,-1)]:
-                if(z+x in range(0,len(image)) and y+w in range(0,len(image[0]))):
-                    if(image[z+x][y+w]==o and (z+x,y+w) not in k): q.append((z+x, y+w))
-        return image 
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        a, b = len(grid), len(grid[0])
+        def dfs(z, y):
+            if not (0<=z<a and 0<=y<b and grid[z][y]==1): return 0
+            grid[z][y], ans = 0, 0
+            for m, n in [(0,1),(1,0),(0,-1),(-1,0)]:
+                ans+=dfs(z+m,y+n)
+            return ans+1 
+        for z in range(a):
+            for y in range(b):
+                if(z*y*(a-z-1)*(b-y-1)==0): dfs(z,y)
+        return sum(sum(z) for z in grid)

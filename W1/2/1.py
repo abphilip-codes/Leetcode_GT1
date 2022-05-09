a9b330dc4154ca76a3c4eaf11da44fdc4b52a8cb
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-09 08:06:07 +0530

    Max Area of Island

--- W1/2/1.py
+++ W1/2/1.py
@@ -2,14 +2,22 @@
 # https://leetcode.com/problems/max-area-of-island/
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        k, q = set(), [(sr, sc)]
-        o = image[sr][sc]
-        while(q):
-            z, y = q.pop(0)
-            k.add((z,y))
-            image[z][y] = newColor
-            for x, w in [(1,0), (0, 1), (-1, 0), (0,-1)]:
-                if(z+x in range(0,len(image)) and y+w in range(0,len(image[0]))):
-                    if(image[z+x][y+w]==o and (z+x,y+w) not in k): q.append((z+x, y+w))
-        return image 
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        a, b = len(grid), len(grid[0])
+        s, ans, k = set(), 0, 0
+        for z in range(a):
+            for y in range(b):
+                arr, k = [(z,y)], 0
+                while(arr):
+                    r, c = arr[-1]
+                    arr.pop(-1)
+                    if r<0 or r>=a or c<0 or c>=b or (r, c) in s or grid[r][c]!=1:
+                        continue
+                    k+=1
+                    s.add((r, c))
+                    arr.append((r-1, c))
+                    arr.append((r+1, c))
+                    arr.append((r, c-1))
+                    arr.append((r, c+1))
+                ans = max(ans, k)
+        return ans

78bcab3f50020f042a5d2b61a9265bda1dadb419
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-10 06:27:41 +0530

    README

---
+++ W1/4/1.py
@@ -0,0 +1,16 @@
+# 1162
+# https://leetcode.com/problems/as-far-from-land-as-possible/
+
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        a, b = len(grid), len(grid[0])
+        def dfs(z, y):
+            if not (0<=z<a and 0<=y<b and grid[z][y]==1): return 0
+            grid[z][y], ans = 0, 0
+            for m, n in [(0,1),(1,0),(0,-1),(-1,0)]:
+                ans+=dfs(z+m,y+n)
+            return ans+1 
+        for z in range(a):
+            for y in range(b):
+                if(z*y*(a-z-1)*(b-y-1)==0): dfs(z,y)
+        return sum(sum(z) for z in grid)
---
+++ W1/5/2.py
@@ -0,0 +1,17 @@
+# 542
+# https://leetcode.com/problems/01-matrix/
+
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        m, n = len(grid2), len(grid2[0])
+        def dfs(z, y):
+            if(z<0 or y<0 or z==m or y==n or grid2[z][y]==0): return True
+            k = True
+            if(grid1[z][y]==0): k = False
+            grid2[z][y] = 0
+            k = dfs(z+1, y) and k
+            k = dfs(z-1, y) and k
+            k = dfs(z, y+1) and k
+            k = dfs(z, y-1) and k
+            return k
+        return sum(grid2[z][y] and dfs(z, y) for z in range(m) for y in range(n))

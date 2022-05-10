4522d7fe2c5226fcaa6a6e233f0feb5eb2437263
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-10 07:15:08 +0530

    As Far from Land as Possible

--- W1/4/1.py
+++ W1/4/1.py
@@ -2,15 +2,20 @@
 # https://leetcode.com/problems/as-far-from-land-as-possible/
 
 class Solution:
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        a, b = len(grid), len(grid[0])
-        def dfs(z, y):
-            if not (0<=z<a and 0<=y<b and grid[z][y]==1): return 0
-            grid[z][y], ans = 0, 0
-            for m, n in [(0,1),(1,0),(0,-1),(-1,0)]:
-                ans+=dfs(z+m,y+n)
-            return ans+1 
-        for z in range(a):
-            for y in range(b):
-                if(z*y*(a-z-1)*(b-y-1)==0): dfs(z,y)
-        return sum(sum(z) for z in grid)
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        q, n = deque(), len(grid)
+        for z in range(n):
+            for y in range(n):
+                if(grid[z][y]):
+                    q.append((z, y))
+                    grid[z][y] = -1
+        ans = -1
+        while(q):
+            x, w = q.popleft()
+            for x1, w1 in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                x2, w2 = x + x1, w + w1
+                if(0<=x2<n and 0<=w2<n and grid[x2][w2]==0):
+                    grid[x2][w2] = grid[x][y] + 1 if grid[x][w] != -1 else grid[x][w] + 2
+                    ans = max(ans, grid[x2][w2])
+                    q.append((x2, w2))
+        return ans

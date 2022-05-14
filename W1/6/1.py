e964e8bd055c8edb3f88e1da23342b537e885e0a
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-14 09:03:20 +0530

    Shortest Bridge

--- W1/6/1.py
+++ W1/6/1.py
@@ -1,21 +1,47 @@
 # 934
 # https://leetcode.com/problems/shortest-bridge/
 
+from collections import deque
 class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        a, b = len(grid), len(grid[0])
-        if(grid[0][0]==1 or grid[-1][-1]): return -1
-        k = [[False for z in range(b+1)] for j in range(a+1)]
-        x1, y1 = [1,-1,0,0,-1,1,1,-1], [0,0,1,-1,-1,1,-1,1]
-        q, k[0][0] = deque(), True
-        q.append((0,0,0))
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        q = deque()
+        r, c = 0, 0
+        for z in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[z][y] == 1:
+                    r, c = z, y
+                    break
+        self.f1(grid, r, c, q)
+        return self.f2(grid, q)
+                 
+    def f1(self, grid: List[List[int]], r: int, c: int, q: deque) -> None:
+        m, n = len(grid), len(grid[0])
+        grid[r][c] = 2
+        s = [(r, c)]
+        while(s):
+            r, c = s.pop()
+            for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                dr = r + dr
+                dc = c + dc
+                if (0<=dr<m and 0<=dc<n):
+                    k = grid[dr][dc]
+                    grid[dr][dc] = 2
+                    if(k==2): continue
+                    elif(k==0): q.append((dr, dc, 0))
+                    else: s.append((dr, dc))
+                        
+    def f2(self, grid: List[List[int]], q: deque) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
         while(q):
-            x, y, d = q.popleft()
-            if(x==a-1 and y==a-1): return d+1
-            for z in range(8):
-                r = x + x1[z]
-                c = y + y1[z]
-                if(r<0 or c<0 or r>a-1 or c>b-1 or k[r][c] or grid[r][c]): continue
-                q.append((r,c,d+1))
-                k[r][c] = True
-        return -1
+            for z in range(len(q)):
+                r, c, t = q.popleft()
+                if(t==1): return ans
+                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    dr = r + dr
+                    dc = c + dc
+                    if(0<=dr<m and 0<=dc<n and grid[dr][dc]!=2):
+                        q.append((dr, dc, grid[dr][dc]))
+                        grid[dr][dc] = 2
+            ans += 1
+        return ans

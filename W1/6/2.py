bf52a743f4a192aece2e76606231a24d94a1d0cc
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-14 08:51:23 +0530

    Nearest Exit from Entrance in Maze

--- W1/6/2.py
+++ W1/6/2.py
@@ -2,26 +2,24 @@
 # https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
 
 class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        d = mat[:]
-        for z in range(m):
-            for y in range(n):
-                if(mat[z][y]!=0):
-                    if(z==0 and y==0): d[z][y] = 10000
-                    elif(z==0): d[z][y] = mat[z][y-1] + 1
-                    elif(y==0): d[z][y] = mat[z-1][y] + 1
-                    else: d[z][y] = min(mat[z-1][y], mat[z][y-1]) + 1
-
-        for z in range(m-1,-1,-1):
-            for y in range(n-1, -1, -1):
-                if(mat[z][y]!=0):
-                    if(z==m-1 and y==n-1):
-                        mat[z][y] = d[z][y]
-                    elif(z==m-1):
-                        mat[z][y] = min(d[z][y], 1+mat[z][y+1])
-                    elif(y==n-1):
-                        mat[z][y] = min(d[z][y], 1+mat[z+1][y])
-                    else:
-                        mat[z][y] = min(d[z][y], 1+min(mat[z+1][y], mat[z][y+1]))
-        return mat
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        m, n = len(maze), len(maze[0])
+        d = ((0,1), (0,-1), (1,0), (-1,0))
+        
+        q = deque([(entrance[0], entrance[1])])
+        k, p = set([(entrance[0], entrance[1])]), 0
+        
+        while(q):
+            for z in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in d: 
+                    dr, dc = dr + r, dc + c
+                    if not (0<=dr<m and 0<=dc<n):
+                        if((r, c)!=(entrance[0], entrance[1])): return p
+                        continue
+                        
+                    if((dr, dc) not in k and maze[dr][dc]=="."):
+                        k.add((dr, dc))
+                        q.append((dr, dc))
+            p+=1        
+        return -1

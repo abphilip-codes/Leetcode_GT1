9e7aa45096ebde3af3f5210b7c0d954f788be0f4
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-31 10:18:43 +0530

    Find Eventual Safe States

--- W2/2/2.py
+++ W2/2/2.py
@@ -2,45 +2,16 @@
 # https://leetcode.com/problems/find-eventual-safe-states/
 
 class Solution:
-    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        self.extraConn = 0
-        self.parent = [-1 for _ in range(n)]        
-        for n1, n2 in connections:
-            self.Union(n1,n2)        
-        n_s = -1
-        for p in self.parent:
-            if p < 0:
-                n_s += 1                
-        if n_s <= self.extraConn:
-            return(n_s)        
-        return -1
-    
-    def Union(self, n1, n2):
-        p1, p2 = self.Find(n1,n2)        
-        
-        if p1 != p2:            
-            if self.parent[p1] < self.parent[p2]:               
-                self.parent[p1] += self.parent[p2]
-                self.parent[n2] = p1
-                self.parent[p2] = p1
-                
-            else:
-                self.parent[p2] += self.parent[p1]
-                self.parent[n1] = p2
-                self.parent[p1] = p2
-                
-        else:            
-            self.extraConn += 1        
-        
-    def Find(self, n1, n2):
-        p1 = n1
-        p2 = n2        
-        while self.parent[p1] >= 0:
-            p1 = self.parent[n1]
-            n1 = p1
-            
-        while self.parent[p2] >= 0:
-            p2 = self.parent[n2]
-            n2 = p2
-            
-        return (p1,p2)
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        def f(k):
+            d[k]=1
+            for z in graph[k]:
+                if(d[z]==0): f(z)
+                if(d[z]==1): return 
+            d[k]=2
+            return 
+        n = len(graph)
+        d = [0]*n
+        for z in range(n):
+            if(d[z]==0): f(z)
+        return [z for z in range(n) if d[z]==2 ] 

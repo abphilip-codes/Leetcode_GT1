102b3a6ddcc02b76e7d458f47de91afac91faeeb
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-31 10:04:09 +0530

    README

---
+++ W2/2/1.py
@@ -0,0 +1,17 @@
+# 1376
+# https://leetcode.com/problems/time-needed-to-inform-all-employees/
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        d = len(isConnected)
+        n, a, b = 0, set(), set(range(d))
+        while(b):
+            n+=1
+            a.add(b.pop())
+            while(a):
+                z = a.pop()
+                for y, t in enumerate(isConnected[z]):
+                    if t and y in b:
+                        a.add(y)
+                        b.remove(y)
+        return n

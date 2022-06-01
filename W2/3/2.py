9032e5ae4002b2407714f77253f803d57e6b6489
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-01 10:25:17 +0530

    Reorder Routes to Make All Paths Lead to the City Zero

--- W2/3/2.py
+++ W2/3/2.py
@@ -2,45 +2,24 @@
 # https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
 
 class Solution:
-    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        self.extraConn = 0
-        self.parent = [-1 for _ in range(n)]        
-        for n1, n2 in connections:
-            self.Union(n1,n2)        
-        n_s = -1
-        for p in self.parent:
-            if p < 0:
-                n_s += 1                
-        if n_s <= self.extraConn:
-            return(n_s)        
-        return -1
-    
-    def Union(self, n1, n2):
-        p1, p2 = self.Find(n1,n2)        
-        
-        if p1 != p2:            
-            if self.parent[p1] < self.parent[p2]:               
-                self.parent[p1] += self.parent[p2]
-                self.parent[n2] = p1
-                self.parent[p2] = p1
-                
-            else:
-                self.parent[p2] += self.parent[p1]
-                self.parent[n1] = p2
-                self.parent[p1] = p2
-                
-        else:            
-            self.extraConn += 1        
-        
-    def Find(self, n1, n2):
-        p1 = n1
-        p2 = n2        
-        while self.parent[p1] >= 0:
-            p1 = self.parent[n1]
-            n1 = p1
-            
-        while self.parent[p2] >= 0:
-            p2 = self.parent[n2]
-            n2 = p2
-            
-        return (p1,p2)
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        self.count = 0
+        dep, arr = {}, {}
+        for x,y in connections:
+            for a,b,d in [[x,y,dep],[y,x,arr]]:
+                if not a in d: d[a] = [b]
+                else: d[a].append(b)
+        visited = set()
+
+        def f(x):
+            if(x in visited): return
+            visited.add(x)
+            if(x in arr):
+                for y in arr[x]: f(y)
+            if(x in dep):
+                for y in dep[x]:
+                    if not y in visited:
+                        self.count+=1
+                        f(y)
+        f(0)
+        return self.count
--- W2/3/3.py
+++ W2/3/3.py
@@ -1,46 +1,26 @@
 # 847
 # https://leetcode.com/problems/shortest-path-visiting-all-nodes/
 
+from collections import defaultdict
 class Solution:
-    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        self.extraConn = 0
-        self.parent = [-1 for _ in range(n)]        
-        for n1, n2 in connections:
-            self.Union(n1,n2)        
-        n_s = -1
-        for p in self.parent:
-            if p < 0:
-                n_s += 1                
-        if n_s <= self.extraConn:
-            return(n_s)        
-        return -1
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        self.graph = defaultdict(list)
+        self.count = 0
+        for edge in connections:
+            self.graph[edge[0]].append(edge[1])
+        self.visited = [0]*n
+        for z in range(n):
+            if not self.visited[z]: self.f(z)
+        return self.count
     
-    def Union(self, n1, n2):
-        p1, p2 = self.Find(n1,n2)        
-        
-        if p1 != p2:            
-            if self.parent[p1] < self.parent[p2]:               
-                self.parent[p1] += self.parent[p2]
-                self.parent[n2] = p1
-                self.parent[p2] = p1
-                
-            else:
-                self.parent[p2] += self.parent[p1]
-                self.parent[n1] = p2
-                self.parent[p1] = p2
-                
-        else:            
-            self.extraConn += 1        
-        
-    def Find(self, n1, n2):
-        p1 = n1
-        p2 = n2        
-        while self.parent[p1] >= 0:
-            p1 = self.parent[n1]
-            n1 = p1
-            
-        while self.parent[p2] >= 0:
-            p2 = self.parent[n2]
-            n2 = p2
-            
-        return (p1,p2)
+    
+    def f(self, start):
+        found = False
+        neighbors = self.graph[start]
+        self.visited[start] = 1
+        for neighbor in neighbors:
+            if not neighbor: found = True
+            if not self.visited[neighbor]:
+                found = self.f(neighbor)
+                if not found: self.count += 1
+        return found

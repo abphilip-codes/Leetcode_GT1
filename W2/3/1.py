55949c8629ae720b94175e90265a2da18fbe9a76
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-01 10:10:50 +0530

    Shortest Path with Alternating Colors

--- W2/3/1.py
+++ W2/3/1.py
@@ -2,16 +2,40 @@
 # https://leetcode.com/problems/shortest-path-with-alternating-colors/
 
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        d = len(isConnected)
-        n, a, b = 0, set(), set(range(d))
-        while(b):
-            n+=1
-            a.add(b.pop())
-            while(a):
-                z = a.pop()
-                for y, t in enumerate(isConnected[z]):
-                    if t and y in b:
-                        a.add(y)
-                        b.remove(y)
-        return n
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        b = collections.defaultdict(set)
+        r = collections.defaultdict(set)
+        for z in redEdges:
+            u, v = z
+            r[u].add(v)
+        for z in blueEdges:
+            u, v = z
+            b[u].add(v)
+        RED  = 0
+        BLUE = 1 
+        q = collections.deque()
+        q.append((0, RED, 0))
+        q.append((0, BLUE, 0))
+        ans = dict(zip(range(n),[-1]*n))
+        visited = set()
+        visited.add((0, RED))
+        visited.add((0, BLUE))
+        
+        while(q):
+            node, par_edge, length = q.popleft()
+            if(ans[node]==-1): ans[node] = length
+            else: ans[node] = min(ans[node], length)
+            
+            if(par_edge == RED):
+                if(node in b):
+                    for child in b[node]:
+                        if((child, BLUE) not in visited):
+                            visited.add((child, BLUE))
+                            q.append((child, BLUE, length + 1))
+            elif(node in r):
+                for child in r[node]:
+                    if((child, RED) not in visited):
+                        visited.add((child, RED))
+                        q.append((child, RED, length + 1))
+        
+        return [ans[x] for x in range(n)]

d2b4d6a57f6dc8deaca5e2b37bd46da31090a4e7
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-02 10:10:37 +0530

    Minimum Jumps to Reach Home

--- W2/4/2.py
+++ W2/4/2.py
@@ -2,24 +2,27 @@
 # https://leetcode.com/problems/minimum-jumps-to-reach-home/
 
 class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        self.count = 0
-        dep, arr = {}, {}
-        for x,y in connections:
-            for a,b,d in [[x,y,dep],[y,x,arr]]:
-                if not a in d: d[a] = [b]
-                else: d[a].append(b)
-        visited = set()
-
-        def f(x):
-            if(x in visited): return
-            visited.add(x)
-            if(x in arr):
-                for y in arr[x]: f(y)
-            if(x in dep):
-                for y in dep[x]:
-                    if not y in visited:
-                        self.count+=1
-                        f(y)
-        f(0)
-        return self.count
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        if not x: return 0
+        
+        threshold = max(forbidden+[x]) + a+b
+        forbidden = set(forbidden)
+        seen = set([0])
+        q = [[0,0]]
+        
+        while(q):
+            pos, steps = q.pop(0)
+            if(pos+a not in forbidden and pos+a not in seen and pos+a<=threshold):
+                if(pos+a==x): return steps+1
+                q.append([pos+a, steps+1])
+                seen.add(pos+a)
+                
+            if(pos-b>0 and pos-b not in forbidden and pos-b not in seen): 
+                if(pos-b==x): return steps+1
+                seen.add(pos-b)
+                
+                if(pos-b+a not in forbidden and pos-b+a not in seen and pos-b+a<=threshold):
+                    if(pos-b+a==x): return steps+2
+                    q.append([pos-b+a, steps+2])
+                    seen.add(pos-b+a)
+        return -1

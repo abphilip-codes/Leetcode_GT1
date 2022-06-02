d5cac9a039b8751cddd3e8da88cfb8c92a061d35
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-02 09:56:56 +0530

    Jump Game III

--- W2/4/1.py
+++ W2/4/1.py
@@ -2,40 +2,18 @@
 # https://leetcode.com/problems/jump-game-iii/
 
 class Solution:
-    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        b = collections.defaultdict(set)
-        r = collections.defaultdict(set)
-        for z in redEdges:
-            u, v = z
-            r[u].add(v)
-        for z in blueEdges:
-            u, v = z
-            b[u].add(v)
-        RED  = 0
-        BLUE = 1 
-        q = collections.deque()
-        q.append((0, RED, 0))
-        q.append((0, BLUE, 0))
-        ans = dict(zip(range(n),[-1]*n))
+    def canReach(self, arr: List[int], start: int) -> bool:
+        q = []
+        q.append([start, arr[start]])
         visited = set()
-        visited.add((0, RED))
-        visited.add((0, BLUE))
-        
-        while(q):
-            node, par_edge, length = q.popleft()
-            if(ans[node]==-1): ans[node] = length
-            else: ans[node] = min(ans[node], length)
-            
-            if(par_edge == RED):
-                if(node in b):
-                    for child in b[node]:
-                        if((child, BLUE) not in visited):
-                            visited.add((child, BLUE))
-                            q.append((child, BLUE, length + 1))
-            elif(node in r):
-                for child in r[node]:
-                    if((child, RED) not in visited):
-                        visited.add((child, RED))
-                        q.append((child, RED, length + 1))
-        
-        return [ans[x] for x in range(n)]
+        visited.add(start)
+        while len(q) > 0:
+            l, v = q.pop(0)
+            if(v==0): return True
+            if(l-v>=0 and (l-v) not in visited):
+                q.append([l-v, arr[l-v]])
+                visited.add(l-v)               
+            if(l+v<len(arr) and (l+v) not in visited):
+                q.append([l+v, arr[l+v]])
+                visited.add(l+v) 
+        return False

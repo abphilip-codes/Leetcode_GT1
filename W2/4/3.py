a61455631ce94c4451139c8848bddb698b6e55ab
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-02 09:54:40 +0530

    Water and Jug Problem

--- W2/4/3.py
+++ W2/4/3.py
@@ -1,27 +1,7 @@
 # 365
 # https://leetcode.com/problems/water-and-jug-problem/
 
-from math import inf
-from collections import deque
+import math
 class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-        nodeCount = len(graph)
-        masks = [1 << z for z in range(nodeCount)]
-        allVisited = (1 << nodeCount) - 1
-        queue = deque([(z, masks[z]) for z in range(nodeCount)])
-        steps = 0
-        visited_states = [{masks[z]} for z in range(nodeCount)]
-        while(queue):
-            count = len(queue)
-            while(count):
-                currentNode, visited = queue.popleft()
-                if(visited == allVisited): return steps
-                for nb in graph[currentNode]:
-                    new_visited = visited | masks[nb]
-                    if(new_visited==allVisited): return steps + 1
-                    if(new_visited not in visited_states[nb]):
-                        visited_states[nb].add(new_visited)
-                        queue.append((nb, new_visited))
-                count-=1
-            steps+=1
-        return inf
+    def canMeasureWater(self, a: int, b: int, c: int) -> bool:
+        return False if(a+b<c) else True if(a+b==0) else not (c%math.gcd(a, b))

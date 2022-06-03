f2a47c26e6dff76c00f29d539b7fb8035ae33efc
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-03 10:22:10 +0530

    Open the Lock

--- W2/5/2.py
+++ W2/5/2.py
@@ -2,27 +2,45 @@
 # https://leetcode.com/problems/open-the-lock/
 
 class Solution:
-    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
-        if not x: return 0
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if("0000" == target): return 0
+        if("0000" in deadends or target in deadends): return -1
+        v = {}
+        v["0000"] = 0
+        q = collections.deque()
+        q.append("0000")
         
-        threshold = max(forbidden+[x]) + a+b
-        forbidden = set(forbidden)
-        seen = set([0])
-        q = [[0,0]]
+        vr = {}
+        vr[target] = 0
+        qr = collections.deque()
+        qr.append(target)
         
-        while(q):
-            pos, steps = q.pop(0)
-            if(pos+a not in forbidden and pos+a not in seen and pos+a<=threshold):
-                if(pos+a==x): return steps+1
-                q.append([pos+a, steps+1])
-                seen.add(pos+a)
-                
-            if(pos-b>0 and pos-b not in forbidden and pos-b not in seen): 
-                if(pos-b==x): return steps+1
-                seen.add(pos-b)
-                
-                if(pos-b+a not in forbidden and pos-b+a not in seen and pos-b+a<=threshold):
-                    if(pos-b+a==x): return steps+2
-                    q.append([pos-b+a, steps+2])
-                    seen.add(pos-b+a)
+        d = {}
+        for each in deadends: d[each] = True
+
+        def f(val):
+            ans = []
+            for z in range(len(val)):
+                t1 = val[:z] + str((int(val[z])+1)%10) + val[z+1:]
+                t2 = val[:z] + str((int(val[z])-1)%10) + val[z+1:]
+                if t1 not in d: ans.append(str(t1))
+                if t2 not in d: ans.append(str(t2))
+            return ans
+        
+        while q and qr:
+            n = q.popleft()
+            nr = qr.popleft()
+
+            for z in f(n):
+                if z not in v:
+                    v[z] = v[n] + 1
+                    q.append(z)
+                    if z in vr: return vr[z] + v[z]
+            
+            for z in f(nr):
+                if z not in vr:
+                    vr[z] = vr[nr] + 1
+                    qr.append(z)
+                    if z in v: return v[z] + vr[z]
+                    
         return -1

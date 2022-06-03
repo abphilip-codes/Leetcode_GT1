a539573be6d0e416f05d29c00359714a3a08afe0
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-03 11:04:30 +0530

    Minimum Genetic Mutation

--- W2/5/1.py
+++ W2/5/1.py
@@ -2,18 +2,23 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
 
 class Solution:
-    def canReach(self, arr: List[int], start: int) -> bool:
-        q = []
-        q.append([start, arr[start]])
-        visited = set()
-        visited.add(start)
-        while len(q) > 0:
-            l, v = q.pop(0)
-            if(v==0): return True
-            if(l-v>=0 and (l-v) not in visited):
-                q.append([l-v, arr[l-v]])
-                visited.add(l-v)               
-            if(l+v<len(arr) and (l+v) not in visited):
-                q.append([l+v, arr[l+v]])
-                visited.add(l+v) 
-        return False
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        if end not in bank: return -1
+        ans = self.f(start, end, bank, [])
+        if(ans==float('inf')): return -1
+        else: return ans    
+            
+    def d(self, s1, s2, c=0):
+        for z in range(len(s1)):
+            if s1[z]!=s2[z]: c+=1
+            if c>1: return False
+        return c==1
+       
+    def f(self, start, end, bank, path):
+        if self.d(start, end): return 1
+        else:
+            k = float('inf')
+            for z in bank:
+                if(z!=end and self.d(start, z) and z not in path):
+                    k = min(1+self.f(z, end, bank, path+[z]), k)
+            return k

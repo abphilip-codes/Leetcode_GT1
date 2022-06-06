e9dfa64659e20d2ba3c2f68d6afd9e7945f47d6c
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-06 10:33:23 +0530

    Number of Operations to Make Network Connected

--- W2/1/2.py
+++ W2/1/2.py
@@ -3,44 +3,35 @@
 
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        self.extraConn = 0
-        self.parent = [-1 for _ in range(n)]        
-        for n1, n2 in connections:
-            self.Union(n1,n2)        
-        n_s = -1
-        for p in self.parent:
-            if p < 0:
-                n_s += 1                
-        if n_s <= self.extraConn:
-            return(n_s)        
+        self.c = 0
+        self.p = [-1 for _ in range(n)]        
+        for n1, n2 in connections: self.u(n1,n2)        
+        s = -1
+        for z in self.p:
+            if(z<0): s+=1                
+        if(s<=self.c): return(s)        
         return -1
     
-    def Union(self, n1, n2):
-        p1, p2 = self.Find(n1,n2)        
-        
-        if p1 != p2:            
-            if self.parent[p1] < self.parent[p2]:               
-                self.parent[p1] += self.parent[p2]
-                self.parent[n2] = p1
-                self.parent[p2] = p1
-                
+    def u(self, n1, n2):
+        p1, p2 = self.f(n1,n2)        
+        if(p1!=p2):            
+            if(self.p[p1] < self.p[p2]):               
+                self.p[p1] += self.p[p2]
+                self.p[n2] = p1
+                self.p[p2] = p1
             else:
-                self.parent[p2] += self.parent[p1]
-                self.parent[n1] = p2
-                self.parent[p1] = p2
-                
-        else:            
-            self.extraConn += 1        
+                self.p[p2] += self.p[p1]
+                self.p[n1] = p2
+                self.p[p1] = p2
+        else: self.c += 1        
         
-    def Find(self, n1, n2):
+    def f(self, n1, n2):
         p1 = n1
         p2 = n2        
-        while self.parent[p1] >= 0:
-            p1 = self.parent[n1]
-            n1 = p1
-            
-        while self.parent[p2] >= 0:
-            p2 = self.parent[n2]
+        while(self.p[p1]>=0):
+            p1 = self.p[n1]
+            n1 = p1  
+        while(self.p[p2]>=0):
+            p2 = self.p[n2]
             n2 = p2
-            
         return (p1,p2)

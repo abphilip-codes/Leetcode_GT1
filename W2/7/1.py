12acb159abed62b9c1cd4725b823bbe0234aecb4
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-07 09:14:15 +0530

    Maximal Network Rank

--- W2/7/1.py
+++ W2/7/1.py
@@ -4,11 +4,10 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
         if not roads: return 0
-        c = defaultdict(set)
+        c, h = defaultdict(set), []
         for r in roads:
             c[r[0]].add(r[1])
             c[r[1]].add(r[0])
-        h = []
         for k1, k2 in c.items():
             heapq.heappush(h, (-len(k2), k1, tuple(k2)))
         a1 = heapq.heappop(h)
@@ -22,6 +21,6 @@ class Solution:
             for y in range(z+1, len(p)):
                 c1, c2 = p[z], p[y]
                 l = c1[1] in c2[2]
-                d = -c1[0] + (-c2[0]) - l
-                if(d==-b1+-b2): return d
-        return -b1+-b2-1
+                d = -c1[0]-c2[0]-l
+                if(d==-b1-b2): return d
+        return -b1-b2-1

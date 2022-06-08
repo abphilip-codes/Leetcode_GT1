aef226b37faf96bd119476e60f24396c596da137
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-08 07:38:51 +0530

    Possible Bipartition

--- W2/7/2.py
+++ W2/7/2.py
@@ -3,24 +3,21 @@
 
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        enemies = [[] for _ in range(n + 1)]
-        for dislike in dislikes:
-            enemies[dislike[0]].append(dislike[1]) 
-            enemies[dislike[1]].append(dislike[0])
+        e = [[] for _ in range(n+1)]
+        for d in dislikes:
+            e[d[0]].append(d[1]) 
+            e[d[1]].append(d[0])
         
-        colors = [0] * (n + 1)
-        for person in range(1, n + 1):
-            if colors[person]:
-                continue
+        c = [0]*(n+1)
+        for p in range(1, n+1):
+            if c[p]: continue
             
-            stack = [person]
-            colors[person] = 1
-            while stack:
-                p = stack.pop()
-                for enemy in enemies[p]:
-                    if colors[enemy] == colors[p]:
-                        return False
-                    if not colors[enemy]:
-                        colors[enemy] = 1 if colors[p] == 2 else 2
-                        stack.append(enemy)
+            s, c[p] = [p], 1
+            while(s):
+                p = s.pop()
+                for z in e[p]:
+                    if(c[z]==c[p]): return False
+                    if not c[z]:
+                        c[z] = 1 if(c[p]==2) else 2
+                        s.append(z)
         return True
